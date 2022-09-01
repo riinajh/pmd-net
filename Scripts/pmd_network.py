95b54a647ed86cb21cd22a20a614e06e983ad538
Jacob Riina <jhriina@gmail.com>
2022-08-31 22:11:18 -0700

    update to centrality calc

--- Scripts/pmd_network.py
+++ Scripts/pmd_network.py
@@ -12,12 +12,11 @@ all_articles={}
 os.chdir('..')
 os.chdir('pmd_baseline\pmd_filtered')
 for file in os.scandir():
-    if 'Index' in file.name:
-        pubmedentries=bz2.BZ2File(file, 'r')
-        index=pickle.load(pubmedentries)
-        for article in index:
-            node_name=article['PMID']
-            all_articles[node_name]=article
+    pubmedentries=bz2.BZ2File(file, 'r')
+    index=pickle.load(pubmedentries)
+    for article in index:
+        node_name=article['PMID']
+        all_articles[node_name]=article
 #   loading in the selected articles and decompressing
 #   Creating keys for each pmid - use to construct graph
 
@@ -38,7 +37,8 @@ def filter_singletons(graph):
 
     Parameters
     ----------
-    graph : the graph of all entries by their PMIDs
+    graph : the graph of all entries by their PMIDs. removes all singleton nodes
+            from the network
 
     Returns
     -------
@@ -71,7 +71,7 @@ def filter_singletons(graph):
 
 #nothing much left to do now but load up everything...
 # for final filter, try looking at neighbors of neighbors,...& etc to root out small
-#disconnected networks. then, assess nodes by accessibility metrics (SAWs?). also research
+#disconnected networks. then, assess nodes by accessibility metrics. also research
 #centrality measurements
 firstfilter=filter_singletons(graph=Net)
 # longest=[{'0'}]
@@ -107,15 +107,20 @@ for item in nx.connected_components(nx.Graph(Net)):
     postfilter_nodes+=len(item)
     print(item)
 print ('Number of subgraphs passing filter:',postfilter_subgraphs,'/',total_subgraphs)
-print('Nodes represented:',postfilter_nodes,'/',firstfilter[0])
-# as it turns out (from actually reading the documentation) you can just use max(nx.conncomp)
+print('Nodes represented:',postfilter_nodes,'/',firstfilter[0], '('+str(round(postfilter_nodes/firstfilter[0],3)*100)+'%)')
+# as it turns out (from actually reading the documentation) you can just use max(nx.connected_components)
 #to find the largest subgraph. since i've already written it this way and I also included
 #the filter of everything else i'm leaving it as is
 def make_subgraphs(Net):
     for item in nx.connected_components(nx.Graph(Net)):
-        print(item)
         subg=nx.subgraph(Net,item)
         yield subg
+max_graph=len(max(nx.connected_components(Net.to_undirected())))
+for item in make_subgraphs(Net):
+    if len(item)==max_graph:
+          Net=item
+#Net is now equal to the single largest graph, which hopefully represents the vast 
+#   majority of nodes
 #nx.draw_networkx(Net)
 # ^this is useless, creates a hairball
 
@@ -126,21 +131,47 @@ def make_subgraphs(Net):
 #like, 10 nodes or something like that
 
 #ok now to assess importantness of each node within the graph.
-def most_central(Net):
-    #there are many ways to do this and they are all computationally expensive
-    x=nx.betweenness_centrality(Net)
-    central=max(x, key=lambda key:x[key])
-        #this returns the key of the highest centrality valued node
-    #what if I wanted an ordered list?
+#there are many ways to do this and they are all computationally expensive
+#x=nx.betweenness_centrality(Net)
+    #do not do this^
+#i'm thinking; we can get some measure of network centrality by looking only
+#at shortest paths from source (indegree=0) to destination (outdegree=0) nodes,
+#which is a lot less intensive than calculating betweenness for every single pair...
+s=set()
+t=set()
+lengths=[]
+for node in Net:
+    try:
+        if Net.in_degree(node)==0 and int(all_articles[node]['date'][0])<1970:
+            s.add(node)
+        elif Net.out_degree(node)==0 and int(all_articles[node]['date'][0])>2010:
+            t.add(node)
+    except ValueError:
+        continue
+#more pairs are better but I can't afford to have this thing run for more than like 12 hours
+visited={}
+uniNet=Net.to_undirected()
+def find_paths(uniNet,s,t,visited):
     i=0
-    for thingy in all_articles:
-        #i know, i know, my naming conventions are horrible
-        morestuff=all_articles[thingy]
-        if central in morestuff['citations']:
-            i+=1
-    print('the most central node is',central+ ', which is cited',i,'times')
-#centrality=most_central(Net)
-#this takes a min or two to compute...or ten
+    for source in s:
+        i+=1
+        print(i)
+        for target in t:
+             l,path=nx.bidirectional_dijkstra(uniNet, source, target)
+             path.pop(l)
+             path.pop(0)
+             yield path
+for path in find_paths(uniNet,s,t,visited):
+    for node in path:
+        if not node in visited:
+            visited[node]=1
+        else:
+            visited[node]+=1
+
+central=max(visited, key=lambda key:visited[key])
+    #this returns the key of the highest centrality valued node
+i=Net.in_degree(central)
+print('the most central node is',central+ ', which is cited',i,'times')
 
 download_graph=bz2.BZ2File('Net','w')
 pickle.dump(Net,download_graph)
@@ -148,7 +179,7 @@ download_graph.close()
 download_all_articles=bz2.BZ2File('index','w')
 pickle.dump(all_articles, download_all_articles)
 download_all_articles.close()
-#download_centrality=bz2.BZ2File('Centrality','w')
-#pickle.dump(centrality, download_centrality)
-#download_centrality.close()
+download_centrality=bz2.BZ2File('Centrality','w')
+pickle.dump(visited, download_centrality)
+download_centrality.close()
 pubmedentries.close()
