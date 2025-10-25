6b03b5b6314aed9a9dd2051c8f908caf8a5f2e25
riinajh <jhriina@gmail.com>
2025-10-25 20:56:28 +0200

    MEsH searching complete

--- src/01_query.sh
+++ src/01_query.sh
@@ -1,39 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Usage: ./01_query.sh "term1" "term2" ...
 
-RUNTIME=`date "+%Y%M%d%H%m"`
+RUNTIME=`date "+%Y%m%d%H%M"`
 
 # Exit immediately on errors
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $SCRIPT_DIR
+
 # Directory to store results
 OUTDIR="../data/pubmed_results"
-mkdir -p "$OUTDIR"
+mkdir -p $OUTDIR
 
 # Log file
 LOGFILE="$OUTDIR/log01_${RUNTIME}.txt"
-touch "$LOGFILE"
+touch $LOGFILE
 
 # Output file
-OUTFILE="${OUTDIR}/prefilter_${RUNTIME}.xml.gz"
+OUTFILE="${OUTDIR}/prefilter_${RUNTIME}.xml"
+touch $OUTFILE
 
 # Loop over all provided MeSH terms
 SEARCH_STRING=""
 for TERM in "$@"; do
-    CLEAN_TERM=$(echo "TERM" | xargs | tr ' ' ' ')
-    if [[ -z "$SEARCH_STRING" ]]; then
-	SEARCH_STRING="${CLEAN_TERM}[MESH]"
+    if [ -z $SEARCH_STRING ]; then
+    	SEARCH_STRING="$TERM [MESH]"
     else
-        SEARCH_STRING="${SEARCH_STRING} OR ${CLEAN_TERM}[MESH]"
+        SEARCH_STRING="${SEARCH_STRING} OR $TERM [MESH]"
     fi
 done
 
-echo "[$(date)] Fetching results for MeSH term(s): $SEARCH_STRING" | tee -a "$LOGFILE"
+echo "[$(date)] Fetching results for MeSH term(s): $SEARCH_STRING" | tee -a $LOGFILE
 
 # Perform search with EDirect and create prefilter.xml.gz
-xsearch -query "SEARCH_STRING" | \
-    xfetch | \
-    gzip > "$OUTFILE"
+echo works > $OUTFILE
+xsearch -query $SEARCH_STRING >> $OUTFILE
 	
-echo "[$(date)] Saved ${OUTFILE}" | tee -a "$LOGFILE"
+echo "[$(date)] Saved ${OUTFILE}" | tee -a $LOGFILE
 
